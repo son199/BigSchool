a115042b4c3a30eae004c28b4efd40f251fc4ed2
Administrator <Administrator@PC035>
2019-03-26 11:17:34 +0700

    cuoi gio hoc

--- Bigschool/Controllers/CoursesController.cs
+++ Bigschool/Controllers/CoursesController.cs
@@ -1,17 +1,31 @@
-﻿using System;
+﻿using Bigschool.Models;
+using Bigschool.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
 namespace Bigschool.Controllers
-{
+{ 
+   
     public class CoursesController : Controller
     {
+        private readonly ApplicationDbContext _dbContext;
+        public CoursesController()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
         // GET: Courses
         public ActionResult Create()
         {
-            return View();
+            var viewModel = new CoursesViewModel
+            {
+                Categories = _dbContext.Categories.ToList()
+            };
+            return View(viewModel);
         }
+       
     }
+  
 }
--- Bigschool/ViewModels/CoursesViewModel.cs
+++ Bigschool/ViewModels/CoursesViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Bigschool.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,5 +8,15 @@ namespace Bigschool.ViewModels
 {
     public class CoursesViewModel
     {
+        public string Place { get; set; }
+        public string Date { get; set; }
+        public string Time { get; set; }
+        public byte Category { get; set; }
+        public IEnumerable<Category> Categories { get; set; }
+        public DateTime GetDateTime()
+        {
+            return DateTime.Parse(string.Format("{0},{1}", Date, Time));
+
+        }
     }
 }
